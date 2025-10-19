e5ad92d060c534c1879981842ae6d52a7fa562d3
SaprZheks <sapronovevgeniy1@gmail.com>
2025-10-19 23:41:30 +0300

    fix bat

--- Obsidian.bat
+++ Obsidian.bat
@@ -1,3 +1,4 @@
 @echo off
 powershell -Command "git pull origin main"
-powershell -Command "Start-Process \"C:\Program Files\Obsidian\Obsidian.exe\" -WindowStyle Hidden"
+start "" "C:\Program Files\Obsidian\Obsidian.exe"
+exit
