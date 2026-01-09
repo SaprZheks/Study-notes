ed5271129dbff874585f6b085ebaeb5c5fac1d73
SaprZheks <sapronovevgeniy1@gmail.com>
2026-01-09 03:58:59 +0300

    .bat

--- Obsidian.bat
+++ Obsidian.bat
@@ -1,5 +1,5 @@
 @echo off
 powershell -Command "git stash"
 powershell -Command "git pull origin main"
-start "" "C:\Program Files\Obsidian\Obsidian.exe"
+call run.bat
 exit
---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "obsidian://open"
+exit
