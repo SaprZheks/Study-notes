9808f6306578972ff02cc11a4771cc004120c0c4
SaprZheks <sapronovevgeniy1@gmail.com>
2026-03-08 01:34:56 +0300

    Обновил скрипт запуска, добавил центровку фото-ссылок, новая заметка про рычаг

--- fix.bat
+++ fix.bat
@@ -1,5 +1,4 @@
 @echo off
 powershell -Command "git stash"
 powershell -Command "git pull origin main"
-call run.bat
 exit
--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 @echo off
+powershell -Command "git submodule foreach --recursive 'git stash'"
+powershell -Command "git submodule update --init --remote --recursive"
 start "" "obsidian://open"
 exit
